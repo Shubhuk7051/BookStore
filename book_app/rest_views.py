791208aa9acfe37bbc517440caf057ea898f329b
Shubham Katre <shubhamkatre7051@gmail.com>
2024-09-03 15:25:54 +0530

    Update

--- book_app/rest_views.py
+++ book_app/rest_views.py
@@ -5,14 +5,12 @@ from .serializers import *
 class BookListView(generics.ListCreateAPIView):
     queryset=Book.objects.all()
     serializer_class=BookSerializer
-    
+
 class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
-    
     queryset=Book.objects.all()
     serializer_class=BookSerializer
     
 class OrderListView(generics.RetrieveUpdateDestroyAPIView):
-    
     queryset=Order.objects.all()
     serializer_class=OrderSerializer
     lookup_field='customer'
